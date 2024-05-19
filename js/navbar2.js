fd7c4739192a446b767e6fdbabf5d1e847e67cfd
mromina00 <mromina03@gmail.com>
2024-05-19 11:19:40 -0300

    nosotros terminado

--- js/navbar2.js
+++ js/navbar2.js
@@ -1,13 +1,13 @@
-let cad = `
+let cad2 = `
     <nav class="nav">
         <a href="index.html" class="logo">
-          <img src="./img/logonavbar.webp" alt="logo dangel motors">
+          <img src="../img/logonavbar.webp" alt="logo dangel motors">
         </a>
         <ul class="navbar">
-          <li><a href="./index.html">Inicio</a></li>
-          <li><a href="./pages/nosotros.html">Sobre Nosotros</a></li>
-          <li><a href="./pages/catalogo.html">Vehículos</a></li>
-          <li><a href="./pages/contacto.html">Contacto</a></li>
+          <li><a href="../index.html">Inicio</a></li>
+          <li><a href="./nosotros.html">Sobre Nosotros</a></li>
+          <li><a href="./catalogo.html">Vehículos</a></li>
+          <li><a href="./contacto.html">Contacto</a></li>
         </ul>
         <!-- ícono menú desplegable -->
         <button class="navtoggle">
@@ -16,15 +16,15 @@ let cad = `
     </nav>
     <div class="menu-desplegable">
         <div class="li">
-            <li><a href="./index.html">Inicio</a></li>
-            <li><a href="./pages/nosotros.html">Nosotros</a></li>
-            <li><a href="./pages/catalogo.html">Vehículos</a></li>
-            <li><a href="./pages/contacto.html">Contacto</a></li>
+            <li><a href="../index.html">Inicio</a></li>
+            <li><a href="./nosotros.html">Nosotros</a></li>
+            <li><a href="./vehiculos.html">Vehículos</a></li>
+            <li><a href="./contacto.html">Contacto</a></li>
         </div>
     </div>
 `
 
-document.querySelector("header").innerHTML = cad
+document.querySelector(".header2").innerHTML = cad2
 
 //Menú Desplegable
 const navtoggle = document.querySelector('.navtoggle')
--- js/sidenav.js
+++ js/sidenav.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "150px";
-    document.getElementById("catalogoDeAutos").style.marginLeft = "150px";
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("catalogoDeAutos").style.marginLeft = "250px";
+    document.getElementById("body").classList.add("overlay-active");
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("catalogoDeAutos").style.marginLeft = "0";
+    document.getElementById("body").classList.remove("overlay-active");
   }
